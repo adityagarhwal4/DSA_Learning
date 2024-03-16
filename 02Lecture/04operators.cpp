05ed7dbd284d3931133dba133e62d0123b19bd5c
adityagarhwal4 <adityagarhwal4@gmail.com>
2024-03-16 11:22:54 +0530

    lecture 2 completed

---
+++ 02Lecture/04operators.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // OPERATORS
+    // Arthmatic operators
+
+    // + - * /
+    // int a = 2.0 / 5;
+    // cout << a << endl;
+    // cout << 2.0 / 5 << endl;
+
+    // RELATIONAL OPERATOR
+    // =
+    // >
+    // <
+    // >=
+    // <=
+    // ==
+    // !=
+
+    // int a = 2;
+    // int b = 3;
+
+    // bool first = (a == b);
+    // cout << first << endl;
+
+    // bool second = (a > b);
+    // cout << second << endl;
+
+    // bool third = (a < b);
+    // cout << third << endl;
+
+    // bool fourth = (a >= b);
+    // cout << fourth << endl;
+
+    // bool fifth = (a <= b);
+    // cout << fifth << endl;
+
+    // bool sixth = (a != b);
+    // cout << sixth << endl;
+
+    // ASSIGNMENT OPERATOR
+    // =
+
+    // LOGICAL OPERATOR
+    // && || !
+
+
+    // BITWISE OPERATOR
+    // understand later
+}
